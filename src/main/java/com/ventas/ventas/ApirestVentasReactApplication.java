b412c2756879b1508ab117c902bd393546724fb3
Jorge Meza Bello <jorberiver@gmail.com>
2022-09-25 21:12:14 -0500

    service listo

---
+++ src/main/java/com/ventas/ventas/ApirestVentasReactApplication.java
@@ -0,0 +1,13 @@
+package com.ventas.ventas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestVentasReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestVentasReactApplication.class, args);
+	}
+
+}

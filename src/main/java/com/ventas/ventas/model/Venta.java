5552bbdc269d61ea8675eee0480511cd59a61430
Jorge Meza Bello <jorberiver@gmail.com>
2022-10-11 23:45:40 -0500

    Modelo de venta realizado

---
+++ src/main/java/com/ventas/ventas/model/Venta.java
@@ -0,0 +1,60 @@
+package com.ventas.ventas.model;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Venta {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer idVenta;
+	private LocalDate fecha;
+	@ManyToOne
+	@JoinColumn(name="id_cliente", nullable = false)
+	private Cliente cliente;
+	
+	//GETTERS AND SETTERS
+	public Integer getIdVenta() {
+		return idVenta;
+	}
+	public void setIdVenta(Integer idVenta) {
+		this.idVenta = idVenta;
+	}
+	public LocalDate getFecha() {
+		return fecha;
+	}
+	public void setFecha(LocalDate fecha) {
+		this.fecha = fecha;
+	}
+	public Cliente getCliente() {
+		return cliente;
+	}
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(idVenta);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Venta other = (Venta) obj;
+		return Objects.equals(idVenta, other.idVenta);
+	}
+	
+	
+}

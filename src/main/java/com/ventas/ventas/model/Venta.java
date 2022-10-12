761ac057effce079c6ca330d4f8555abe8494ada
Jorge Meza Bello <jorberiver@gmail.com>
2022-10-12 16:40:25 -0500

    Relacion detalle ventas ventas producto listo

--- src/main/java/com/ventas/ventas/model/Venta.java
+++ src/main/java/com/ventas/ventas/model/Venta.java
@@ -1,14 +1,18 @@
 package com.ventas.ventas.model;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Objects;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Venta {
@@ -20,6 +24,8 @@ public class Venta {
 	@ManyToOne
 	@JoinColumn(name="id_cliente", nullable = false)
 	private Cliente cliente;
+	@OneToMany(mappedBy = "venta", cascade = {CascadeType.PERSIST, CascadeType.REMOVE}, fetch = FetchType.LAZY )
+	private List<DetalleVenta> detalleVentaList;
 	
 	//GETTERS AND SETTERS
 	public Integer getIdVenta() {

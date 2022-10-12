f2aa9455922b87959e30009eff7f7ccaed1a8307
Jorge Meza Bello <jorberiver@gmail.com>
2022-10-11 19:26:49 -0500

    Cambios hechos en tabla clientes y creacion del modelo de persona

--- src/main/java/com/ventas/ventas/model/Cliente.java
+++ src/main/java/com/ventas/ventas/model/Cliente.java
@@ -22,23 +22,6 @@ public class Cliente {
 	@NotBlank(message = "El nombre es obligatorio")
 	@Size(min = 3, max=70, message = "El nombre debe ser mayor a tres caracteres")
 	@Column(name="nombre", nullable = false, length = 70)
-	private String nombre;
-	@NotNull
-	@NotBlank(message = "Los apellidos son obligatorio")
-	@Size(min = 3, max=150, message = "Los apellidos deben ser mayores a tres caracteres")
-	@Column(name="apellidos", nullable = false, length = 150)
-	private String apellido;
-	@Size(max = 150, message = "La dirección no debe de superar los 150 caracteres")
-	@Column(name="direccion", nullable = true, length = 150)
-	private String direccion;
-	@Size(min = 10, message = "El teléfono debe tener al menos 10 digitos")
-	@Column(name="telefono", nullable = true, length = 12)
-	private String telefono;
-	@NotNull
-	@NotBlank(message = "El email es obligatorio")
-	@Size(min = 10, message = "El email debe ser de al menos 10 caracteres")
-	@Email(message = "Esto no parece un email")
-	@Column(name="email", nullable = false, length = 70)
 	private String email;
 	public Integer getIdCliente() {
 		return idCliente;
@@ -46,30 +29,7 @@ public class Cliente {
 	public void setIdCliente(Integer idCliente) {
 		this.idCliente = idCliente;
 	}
-	public String getNombre() {
-		return nombre;
-	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	public String getApellido() {
-		return apellido;
-	}
-	public void setApellido(String apellido) {
-		this.apellido = apellido;
-	}
-	public String getDireccion() {
-		return direccion;
-	}
-	public void setDireccion(String direccion) {
-		this.direccion = direccion;
-	}
-	public String getTelefono() {
-		return telefono;
-	}
-	public void setTelefono(String telefono) {
-		this.telefono = telefono;
-	}
+	
 	public String getEmail() {
 		return email;
 	}
---
+++ src/main/java/com/ventas/ventas/model/Persona.java
@@ -0,0 +1,59 @@
+package com.ventas.ventas.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Persona {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer idPersona;
+	@NotNull(message = "El nombre es un campo obligatorio")
+	@NotBlank(message = "El nombre es obligatorio")
+	@Size(min = 3, max=70, message = "El nombre debe ser mayor a tres caracteres")
+	@Column(name="nombre", nullable = false, length = 70)
+	private String nombre;
+	@NotNull
+	@NotBlank(message = "Los apellidos son obligatorio")
+	@Size(min = 3, max=150, message = "Los apellidos deben ser mayores a tres caracteres")
+	@Column(name="apellido", nullable = false, length = 150)
+	private String apellido;
+	@Size(max = 150, message = "La dirección no debe de superar los 150 caracteres")
+	@Column(name="direccion", nullable = true, length = 150)
+	private String direccion;
+	@Size(min = 10, message = "El teléfono debe tener al menos 10 digitos")
+	@Column(name="telefono", nullable = true, length = 12)
+	private String telefono;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+}
